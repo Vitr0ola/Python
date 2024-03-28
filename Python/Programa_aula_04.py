8633f50184e521d865cc6d04427f9421fc3d486e
Vitor Santos <147724409+Vitr0ola@users.noreply.github.com>
2024-03-28 16:27:24 -0300

    Create Programa_aula_04.py
    Aqui fiz um programa usando bibliotecas e api rest para localizar informações de seu cep utilizando o site viacep.com.br

---
+++ Python/Programa_aula_04.py
@@ -0,0 +1,21 @@
+programa :
+
+# https://viacep.com.br/ws/09180-330/json/
+
+import requests
+import warnings
+warnings.filterwarnings("ignore")
+
+cep = input("Informe o seu CEP")
+url = f'https://viacep.com.br/ws/{cep}/json/'
+
+ret = requests.get(url,verify = False).json()
+dados = ret
+print('-'*80)
+
+print(f"Rua {dados['logradouro']}")
+print(f"Bairro {dados['bairro']}")
+print(f"Cidade {dados['localidade']}")
+
+print('-'*80)
+
